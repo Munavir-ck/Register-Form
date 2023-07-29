0634b31bd19b2e15883075734c31adfcd0131798
Munavir <munavirokv@gmail.com>
2023-07-29 12:03:46 +0530

    edited

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -38,24 +38,24 @@ function Signup() {
   return (
     <div>
           <ToastContainer />
-      <div class="min-h-screen p-6 bg-gray-100 flex items-center justify-center">
-        <div class="container max-w-screen-lg mx-auto">
+      <div className="min-h-screen p-6 bg-gray-100 flex items-center justify-center">
+        <div className="container max-w-screen-lg mx-auto">
           <div>
-            <h2 class="font-semibold text-xl text-gray-600">Responsive Form</h2>
-            <p class="text-gray-500 mb-6">
+            <h2 className="font-semibold text-xl text-gray-600">Responsive Form</h2>
+            <p className="text-gray-500 mb-6">
               Form is mobile responsive. Give it a try.
             </p>
 
-            <div class="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
-              <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
-                <div class="text-gray-600">
-                  <p class="font-medium text-lg">Personal Details</p>
+            <div className="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
+              <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
+                <div className="text-gray-600">
+                  <p className="font-medium text-lg">Personal Details</p>
                   <p>Please fill out all the fields.</p>
                 </div>
 
-                <form class="lg:col-span-2" onSubmit={handleSubmit}>
-                  <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
-                    <div class="md:col-span-5">
+                <form className="lg:col-span-2" onSubmit={handleSubmit}>
+                  <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
+                    <div className="md:col-span-5">
                       <label for="full_name">Full Name</label>
                       {errors.name && touched.name && (
                     <p className="text-red-600">{errors.name}</p>
@@ -67,12 +67,12 @@ function Signup() {
                         onBlur={handleBlur}
                         type="text"
                         id="full_name"
-                        class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                        className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
                         placeholder="Name"
                       />
                     </div>
 
-                    <div class="md:col-span-5">
+                    <div className="md:col-span-5">
                       <label for="email">Email Address</label>
                       {errors.email && touched.email && (
                     <p className="text-red-600">{errors.email}</p>
@@ -84,21 +84,21 @@ function Signup() {
                         onBlur={handleBlur}
                         type="email"
                         id="email"
-                        class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                        className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
                         placeholder="email@domain.com"
                       />
                     </div>
 
-                    <div class="md:col-span-3">
+                    <div className="md:col-span-3">
                       <label for="address">Address / Street</label>
                       {errors.phone && touched.address && (
-                    <p className="text-red-600">{errors.address}</p>
+                    <p classNamee="text-red-600">{errors.address}</p>
                   )}
                       <input
                         type="text"
                         name="address"
                         id="address"
-                        class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                        className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
                         placeholder="address"
                         value={values.address}
                         onChange={handleChange}
@@ -106,10 +106,10 @@ function Signup() {
                       />
                     </div>
 
-                    <div class="md:col-span-2">
+                    <div className="md:col-span-2">
                       <label for="city">Phone</label>
                       {errors.phone && touched.phone && (
-                    <p className="text-red-600">{errors.phone}</p>
+                    <p classNamee="text-red-600">{errors.phone}</p>
                   )}
                       <input
                         type="number"
@@ -117,20 +117,20 @@ function Signup() {
                         value={values.phone}
                         onChange={handleChange}
                         onBlur={handleBlur}
-                        class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                        className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
                         placeholder="phone"
                       />
                     </div>
 
-                    <div class="md:col-span-2">
+                    <div className="md:col-span-2">
                       <label for="country">Password</label>
                       {errors.password && touched.password && (
-                    <p className="text-red-600">{errors.password}</p>
+                    <p classNamee="text-red-600">{errors.password}</p>
                   )}
-                      <div class="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
+                      <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
                         <input
                           placeholder="password"
-                          class="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
+                          className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
                           name="password"
                           value={values.password}
                           onChange={handleChange}
@@ -139,15 +139,15 @@ function Signup() {
                       </div>
                     </div>
 
-                    <div class="md:col-span-2">
+                    <div className="md:col-span-2">
                       <label for="state">Confirm Password</label>
                       {errors.cpassword && touched.cpassword && (
-                    <p className="text-red-600">{errors.cpassword}</p>
+                    <p classNamee="text-red-600">{errors.cpassword}</p>
                   )}
-                      <div class="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
+                      <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
                         <input
                           placeholder="confrim password"
-                          class="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
+                          className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
                           name="cpassword"
                           value={values.cpassword}
                           onChange={handleChange}
@@ -156,10 +156,10 @@ function Signup() {
                       </div>
                     </div>
 
-                    <div class="md:col-span-5 text-right">
-                      <div class="inline-flex items-end">
+                    <div className="md:col-span-5 text-right">
+                      <div className="inline-flex items-end">
                         <button
-                          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                           type="submit"
                         >
                           Submit
